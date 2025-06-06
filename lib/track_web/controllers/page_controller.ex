828d552d69bcedc483b8dae2eeaa3fa8f0c889b0
Kitasio <kit.plotits@gmail.com>
2025-06-06 14:28:46 +0200

    First

---
+++ lib/track_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule TrackWeb.PageController do
+  use TrackWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
