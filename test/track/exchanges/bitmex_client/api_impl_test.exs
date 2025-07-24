15705ee543eba8e619ec65fa4b872e5205a742af
Kitasio <kit.plotits@gmail.com>
2025-07-24 15:08:43 +0200

    Refactor test

--- test/track/exchanges/bitmex_client/api_impl_test.exs
+++ test/track/exchanges/bitmex_client/api_impl_test.exs
@@ -7,20 +7,25 @@ defmodule Track.Exchanges.BitmexClient.APIImplTest do
   alias Track.Exchanges.BitmexClient.APIImpl
 
   setup do
-    bypass = Bypass.open()
-    # clean up application env after test
-    on_exit(fn -> Application.delete_env(:track, :bitmex_api_base_url) end)
-    {:ok, bypass: bypass}
-  end
-
-  test "get_balance/2 makes a correct request and handles the response", %{bypass: bypass} do
     scope = user_scope_fixture()
     bitmex_setting = bitmex_setting_fixture(scope)
+    bypass = Bypass.open()
 
     # Configure the application to use the bypass URL for this test
     base_url = "http://localhost:#{bypass.port}"
     Application.put_env(:track, :bitmex_api_base_url, base_url)
 
+    # clean up application env after test
+    on_exit(fn -> Application.delete_env(:track, :bitmex_api_base_url) end)
+
+    {:ok, bypass: bypass, scope: scope, bitmex_setting: bitmex_setting}
+  end
+
+  test "get_balance/2 makes a correct request and handles the response", %{
+    bypass: bypass,
+    scope: scope,
+    bitmex_setting: bitmex_setting
+  } do
     currency = "XBt"
     path = "/api/v1/user/margin"
     query = "currency=#{currency}"
@@ -35,20 +40,7 @@ defmodule Track.Exchanges.BitmexClient.APIImplTest do
     ]
     """
 
-    Bypass.expect_once(bypass, "GET", path, fn conn ->
-      assert conn.query_string == query
-
-      api_key_header = Plug.Conn.get_req_header(conn, "api-key")
-      assert api_key_header == [bitmex_setting.api_key]
-
-      api_expires_header = Plug.Conn.get_req_header(conn, "api-expires")
-      assert api_expires_header != []
-
-      api_signature_header = Plug.Conn.get_req_header(conn, "api-signature")
-      assert api_signature_header != []
-
-      Plug.Conn.resp(conn, 200, expected_response_body)
-    end)
+    expect_api_call(bypass, path, query, bitmex_setting, expected_response_body)
 
     assert {:ok,
             [
@@ -60,14 +52,11 @@ defmodule Track.Exchanges.BitmexClient.APIImplTest do
             ]} == APIImpl.get_balance(scope, currency)
   end
 
-  test "get_instrument/2 makes a correct request and handles the response", %{bypass: bypass} do
-    scope = user_scope_fixture()
-    bitmex_setting = bitmex_setting_fixture(scope)
-
-    # Configure the application to use the bypass URL for this test
-    base_url = "http://localhost:#{bypass.port}"
-    Application.put_env(:track, :bitmex_api_base_url, base_url)
-
+  test "get_instrument/2 makes a correct request and handles the response", %{
+    bypass: bypass,
+    scope: scope,
+    bitmex_setting: bitmex_setting
+  } do
     symbol = "XBTUSD"
     path = "/api/v1/instrument"
     query = "symbol=#{symbol}"
@@ -80,20 +69,7 @@ defmodule Track.Exchanges.BitmexClient.APIImplTest do
     ]
     """
 
-    Bypass.expect_once(bypass, "GET", path, fn conn ->
-      assert conn.query_string == query
-
-      api_key_header = Plug.Conn.get_req_header(conn, "api-key")
-      assert api_key_header == [bitmex_setting.api_key]
-
-      api_expires_header = Plug.Conn.get_req_header(conn, "api-expires")
-      assert api_expires_header != []
-
-      api_signature_header = Plug.Conn.get_req_header(conn, "api-signature")
-      assert api_signature_header != []
-
-      Plug.Conn.resp(conn, 200, expected_response_body)
-    end)
+    expect_api_call(bypass, path, query, bitmex_setting, expected_response_body)
 
     assert {:ok,
             [
@@ -101,5 +77,19 @@ defmodule Track.Exchanges.BitmexClient.APIImplTest do
                 "lastPrice" => 1234
               }
             ]} == APIImpl.get_instrument(scope, symbol)
+  end
+
+  defp expect_api_call(bypass, path, query, bitmex_setting, expected_response_body) do
+    Bypass.expect_once(bypass, "GET", path, fn conn ->
+      assert conn.query_string == query
+      assert_auth_headers(conn, bitmex_setting.api_key)
+      Plug.Conn.resp(conn, 200, expected_response_body)
+    end)
+  end
+
+  defp assert_auth_headers(conn, api_key) do
+    assert Plug.Conn.get_req_header(conn, "api-key") == [api_key]
+    assert Plug.Conn.get_req_header(conn, "api-expires") != []
+    assert Plug.Conn.get_req_header(conn, "api-signature") != []
   end
 end
